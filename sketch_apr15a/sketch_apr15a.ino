47f750ac7e4de57d2d88115dabde69c74200ffbf
beatriceToader <acribiebea@gmail.com>
2024-05-22 18:34:24 +0300

    Add code related to the display

--- sketch_apr15a/sketch_apr15a.ino
+++ sketch_apr15a/sketch_apr15a.ino
@@ -3,6 +3,14 @@
 #include <MQTTClient.h>
 #include <ArduinoJson.h>
 #include "WiFi.h"
+#include <Wire.h>  
+#include "HT_SSD1306Wire.h"
+
+#define SDA_OLED 4
+#define SCL_OLED 15
+#define RST_OLED 16
+
+SSD1306Wire  factory_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED); 
 
 // The MQTT topics that this device should publish/subscribe
 #define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
@@ -11,12 +19,21 @@
 WiFiClientSecure net = WiFiClientSecure();
 MQTTClient client = MQTTClient(256);
 
+void logo(){
+	factory_display.clear();
+  factory_display.drawString(15, 15, "STRESS\nDETECTOR");
+	factory_display.display();
+}
+
 void connectAWS()
 {
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   Serial.println("Connecting to Wi-Fi");
+
+  factory_display.drawString(0, 0, "Connecting to Wi-Fi");
+	factory_display.display(); 
 
   while (WiFi.status() != WL_CONNECTED){
     delay(500);
@@ -36,6 +53,10 @@ void connectAWS()
 
   Serial.print("Connecting to AWS IOT");
 
+  factory_display.clear();
+  factory_display.drawString(0, 0, "Connecting to AWS IOT");
+	factory_display.display();
+
   while (!client.connect(THINGNAME)) {
     Serial.print("aws.");
     delay(100);
@@ -43,6 +64,10 @@ void connectAWS()
 
   if(!client.connected()){
     Serial.println("AWS IoT Timeout!");
+
+  factory_display.clear();
+  factory_display.drawString(0, 0, "Connecting to AWS IOT");
+	factory_display.display();
     return;
   }
 
@@ -50,6 +75,9 @@ void connectAWS()
   client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
 
   Serial.println("AWS IoT Connected!");
+  factory_display.clear();
+  factory_display.drawString(0, 0, "AWS IoT Connected!");
+	factory_display.display();
 }
 
 void publishMessage()
@@ -59,21 +87,41 @@ void publishMessage()
   doc["sensor_a0"] = analogRead(0);
   doc["number"] = 12;
   char jsonBuffer[512];
-  serializeJson(doc, jsonBuffer); // print to client
 
+  unsigned long time = doc["time"];
+  int sensor_a0 = doc["sensor_a0"];
+  int number = doc["number"];
+
+  String printString = "Time: " + String(time) + "\n" + "Sensor Value: " + String(sensor_a0) + "\n" + "Number: "+ String(number);
+
+  factory_display.clear();
+  factory_display.drawString(0, 0, printString.c_str());
+	factory_display.display();
+
+  serializeJson(doc, jsonBuffer); 
   client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
 }
 
 void messageHandler(String &topic, String &payload) {
   Serial.println("incoming: " + topic + " - " + payload);
 
-//  StaticJsonDocument<200> doc;
-//  deserializeJson(doc, payload);
-//  const char* message = doc["message"];
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, payload);
+  const char* message = doc["message"];
+
+  factory_display.clear();
+  factory_display.drawString(0, 0, message);
+	factory_display.display();
 }
 
 void setup() {
   Serial.begin(9600);
+
+  factory_display.init();
+  logo();
+
+	delay(300);
+	factory_display.clear();
   connectAWS();
 }
 
